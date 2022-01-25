c8453a242b74873a5ea8002f3d38604cb8c8c998
freshin123 <69437586+freshin123@users.noreply.github.com>
2022-01-25 11:28:35 +0530

    Final

---
+++ StudentResultSystem/src/main/java/StudentSystem/model.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StudentSystem;
+
+/**
+ *
+ * @author freshin
+ */
+class model {
+
+    static void addRow(String[] dataRow) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
